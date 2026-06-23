d38eea86be7dc0096c3bfbc93edaac2b6e94d06b
MahendraKumar <mahendrakumar42945@gmail.com>
2026-06-23 15:30:25 +0530

    initial commit

--- server/config/db.js
+++ server/config/db.js
@@ -2,15 +2,11 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(
-      process.env.MONGO_URI
-    );
+    const conn = await mongoose.connect(process.env.MONGO_URI);
 
-    console.log(
-      `MongoDB Connected: ${conn.connection.host}`
-    );
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error("Database Error:", error.message);
+    console.error("Database Connection Error:", error.message);
     process.exit(1);
   }
 };
--- server/server.js
+++ server/server.js
@@ -1,40 +1,27 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const handleRegister = require("./routes/authRoutes");
-const cartRoutes = require("./routes/cartRoutes");
-const orderRoutes = require("./routes/orderRoutes")
-const paymentRoutes = require("./routes/paymentRoutes");
-const productRoutes = require("./routes/productRoutes");
 const connectDB = require("./config/db");
 
 dotenv.config();
 
 const app = express();
 
+// Connect to Database
 connectDB();
 
 app.use(cors());
 app.use(express.json());
 
 // Routes
-app.use("/api/auth",handleRegister);
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/products", require("./routes/productRoutes"));
+app.use("/api/cart", require("./routes/cartRoutes"));
+app.use("/api/orders", require("./routes/orderRoutes"));
+app.use("/api/payments", require("./routes/paymentRoutes"));
 
-app.use(
-  "/api/products",productRoutes);
+const PORT = process.env.PORT || 5001;
 
-app.use(
-  "/api/cart",cartRoutes);
-
-app.use(
-  "/api/orders",orderRoutes
-);
-
-app.use(
-  "/api/payments",paymentRoutes);
-
-app.listen(5001, () => {
-  console.log(
-    "Server running on port 5001"
-  );
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
